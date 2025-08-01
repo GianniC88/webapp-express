308289700dc4bab4894e46f0b60735e3f1150dd2
GianniC88 <gianni.colucci.88@gmail.com>
2025-08-01 20:27:46 +0200

    feat: add database connection setup with MySQL2

---
+++ backend-api/connection.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const mysql = require('mysql2')
+
+const credentials = {
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+}
+
+const connection = mysql.createConnection(credentials)
+
+connection.connect(err => {
+	if (err) {
+		throw err
+	}
+	console.info('connection successfull')
+})
+
+module.exports = connection
