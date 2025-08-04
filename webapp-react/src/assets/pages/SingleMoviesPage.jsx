70469238dd79ae5f11c20c5163a7b8ad17428156
GianniC88 <gianni.colucci.88@gmail.com>
2025-08-04 08:56:59 +0200

    feat: implement SingleMoviesPage with movie details and reviews display

--- webapp-react/src/assets/pages/SingleMoviesPage.jsx
+++ webapp-react/src/assets/pages/SingleMoviesPage.jsx
@@ -1,7 +1,56 @@
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
+import Jumbotron from "../components/Jumbotron"
+
 export default function SingleMovies() {
+
+
+	const { id } = useParams()
+	const api_server_url = `${import.meta.env.VITE_BACKEND_API_SERVER}/${id}`
+	const [movie, setMovie] = useState({})
+	console.log(id)
+
+	useEffect(() => {
+
+		fetch(api_server_url)
+			.then(res => res.json())
+			.then(data => {
+				console.log(data)
+				setMovie(data)
+			})
+
+	}, [id])
 	return (
 		<>
-			<h1>Single book</h1>
+			<h1>Single Movie</h1>
+			<Jumbotron title={movie.title} text={movie?.plot} cta={`view ${movie?.title}`} />
+
+			<section>
+				<div className="container">
+					<h2>Recensioni</h2>
+
+					{movie.reviews && movie.reviews.length > 0 ? (
+						movie.reviews.map(review => (
+							<div key={review.id} className="card p-3 mb-3">
+								<h5>Recensione di: {review.name}</h5>
+								<div className="mb-2">
+									<strong>Voto:</strong> {review.vote}/5 ⭐
+								</div>
+								<p>{review.text}</p>
+							</div>
+						))
+					) : (
+						<div className="alert alert-info">
+							<h5>Nessuna recensione disponibile</h5>
+							<p>Questo film non ha ancora recensioni.</p>
+						</div>
+					)}
+
+				</div>
+			</section>
+
+
+
 		</>
 	)
 }
