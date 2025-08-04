1c95934ee80a97715c18ad119d623d8f95bc4f13
GianniC88 <gianni.colucci.88@gmail.com>
2025-08-04 15:28:42 +0200

    feat: restructure Jumbotron component layout in HomePage; adjust card review max-width in CSS

--- webapp-react/src/assets/pages/HomePage.jsx
+++ webapp-react/src/assets/pages/HomePage.jsx
@@ -8,24 +8,26 @@ export default function HomePage() {
 	return (
 		<>
 
-			<Jumbotron
-				className="HomePage"
-				title='Movies & GO '
-				text='Scegli il tuo Film e Buona Visione'
-				cta='Scegli il tuo Film'
-				onCtaClick={MovieSelection} />
+			<div className="container">
+				<Jumbotron
+					className="HomePage"
+					title='Movies & GO '
+					text='Scegli il tuo Film e Buona Visione'
+					cta='Scegli il tuo Film'
+					onCtaClick={MovieSelection} />
 
 
-			<div className="card">
+				<div className="card">
 
-				<div className="card-body bg-secondary h-100">
-					<h4 className="card-title display-5 fw-bolder ">Benvenuti nella nostra community</h4>
-					<h4 className="card-text mt-5">Siamo lieti di offrirvi una vasta scelta Film dai Cult alle Nuove uscite
-					</h4>
-					<h4>Mettetevi comodi e Buona Visione</h4>
+					<div className="card-body bg-secondary h-100">
+						<h4 className="card-title display-5 fw-bolder ">Benvenuti nella nostra community</h4>
+						<h4 className="card-text mt-5">Siamo lieti di offrirvi una vasta scelta Film dai Cult alle Nuove uscite
+						</h4>
+						<h4>Mettetevi comodi e Buona Visione</h4>
+					</div>
 				</div>
-			</div>
 
+			</div>
 
 
 
