678c72893aeaffcbec99bd50cd1626dd0041bd53
GianniC88 <gianni.colucci.88@gmail.com>
2025-08-05 19:20:44 +0200

    feat: add middleware for handling 404 and server errors

--- backend-api/server.js
+++ backend-api/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT
 const MoviesRouter = require('./routes/movies')
+const notFound = require('./middleware/notFound')
+const serverError = require('./middleware/serverError')
 app.use('/images', express.static('public/img'))
 
 const cors = require('cors')
@@ -10,7 +12,8 @@ app.use(cors())
 
 app.use('/api/movies', MoviesRouter)
 
-app.use(express.static('piblic'))
+app.use(express.static('public'))
+
 app.listen(PORT, () => {
 	console.log(`Server is Listening http://127.0.0.1:${PORT}`)
 })
@@ -19,3 +22,7 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
 	res.send('my app')
 })
+
+
+app.use(notFound)
+app.use(serverError)
